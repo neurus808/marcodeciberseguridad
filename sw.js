b32a866baa9a08d73bb196bcfa12caca22dcad34
neurus808 <macsi.21@gmail.com>
2025-04-18 22:06:51 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+
+const CACHE_NAME = 'ci-pwa-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/sw.js',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(resp => resp || fetch(event.request))
+  );
+});
